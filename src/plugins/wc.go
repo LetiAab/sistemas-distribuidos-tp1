dd5768a7547082f303bb2f327f525c5ecf2bfcd0
amurseli <agustinmurseli@gmail.com>
2025-09-18 16:42:09 -0300

    Prueba funcional del plugin. Gitignore arreglado.

--- src/plugins/wc.go
+++ src/plugins/wc.go
@@ -1,34 +1,31 @@
 package main
 
 import (
+	"sistemas-distribuidos-tp1/common"  
     "strconv"
     "strings"
     "unicode"
 	"fmt"
 )
 
-type KeyValue struct {
-    Key   string
-    Value string
-}
 
 // Defino así las funcs porque Go carga Plugins dinamicamente, 
 // osea q busca variables públicas. 
-var Map func(string, string) []KeyValue 
+var Map func(string, string) []common.KeyValue 
 var Reduce func(string, []string) string 
 
 
 func init() {
 
-	Map = func(filename string, content string) []KeyValue{
-		var kvs []KeyValue
+	Map = func(filename string, content string) []common.KeyValue{
+		var kvs []common.KeyValue
 
 		// TODO: Esto no separa entre min y mayus creo
 		ff := func(r rune) bool { return !unicode.IsLetter(r) }
 		words := strings.FieldsFunc(content, ff)
 	
 		for _, word := range words{
-			kvs = append(kvs, KeyValue{word, "1"})
+			kvs = append(kvs, common.KeyValue{word, "1"})
 		}
 		
 		return kvs
