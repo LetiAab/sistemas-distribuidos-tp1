1d08fc698d5e063138e2d09efd70996596636240
alendavies <alendavies10@gmail.com>
2025-09-23 13:22:13 -0300

    normalizo palabras

--- src/secuencial.go
+++ src/secuencial.go
@@ -1,79 +1,79 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
-    "sort"
-    "strings"
-    "unicode"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strings"
+	"unicode"
 )
 
 type KeyValue struct {
-    Key   string
-    Value int
+	Key   string
+	Value int
 }
 
 func Map(filename string, content string) []KeyValue {
-    var kvs []KeyValue
-    ff := func(r rune) bool { return !unicode.IsLetter(r) }
-    words := strings.FieldsFunc(content, ff) // separa el string en letras y "no letras"
-    
-    for _, word := range words {
-        kvs = append(kvs, KeyValue{word, 1})
-    }
-    return kvs
+	var kvs []KeyValue
+	ff := func(r rune) bool { return !unicode.IsLetter(r) } // Filtra caracteres no alfabéticos
+	words := strings.FieldsFunc(content, ff)                // Separa el contenido en palabras
+
+	for _, word := range words {
+		normalizedWord := strings.ToLower(word) // Convierte a minúsculas
+		kvs = append(kvs, KeyValue{normalizedWord, 1})
+	}
+	return kvs
 }
 
 func Reduce(key string, values []int) int {
-    sum := 0
-    for _, v := range values {
-        sum += v
-    }
-    return sum
+	sum := 0
+	for _, v := range values {
+		sum += v
+	}
+	return sum
 }
 
 func main() {
-    
-    inputFiles := os.Args[1:]
-    
-    var intermediate []KeyValue
-    for _, filename := range inputFiles {
-        content, err := os.ReadFile(filename)
-        if err != nil {
-            log.Printf("Error leyendo %s: %v\n", filename, err)
-            continue
-        }
-        kvs := Map(filename, string(content))
-        intermediate = append(intermediate, kvs...)
-    }
-    
-    groups := make(map[string][]int)
-    for _, kv := range intermediate { // Agrupamos tal que quede - (Hello, [1, 1])
-        groups[kv.Key] = append(groups[kv.Key], kv.Value)
-    }
-    
-    var results []KeyValue
-    for key, values := range groups {
-        count := Reduce(key, values)
-        results = append(results, KeyValue{key, count})
-    }
-    
-    sort.Slice(results, func(i, j int) bool {
-        return results[i].Key < results[j].Key
-    })
-    
-    outputFile := "mr-out-0"
-    file, err := os.Create(outputFile)
-    if err != nil {
-        log.Fatalf("Error creando archivo de salida: %v", err)
-    }
-    defer file.Close()
-    
-    writer := bufio.NewWriter(file)
-    for _, kv := range results {
-        fmt.Fprintf(writer, "%v %v\n", kv.Key, kv.Value)
-    }
-    writer.Flush()
+	inputFiles := os.Args[1:]
+
+	var intermediate []KeyValue
+	for _, filename := range inputFiles {
+		content, err := os.ReadFile(filename)
+		if err != nil {
+			log.Printf("Error leyendo %s: %v\n", filename, err)
+			continue
+		}
+		kvs := Map(filename, string(content))
+		intermediate = append(intermediate, kvs...)
+	}
+
+	groups := make(map[string][]int)
+	for _, kv := range intermediate { // Agrupamos tal que quede - (Hello, [1, 1])
+		groups[kv.Key] = append(groups[kv.Key], kv.Value)
+	}
+
+	var results []KeyValue
+	for key, values := range groups {
+		count := Reduce(key, values)
+		results = append(results, KeyValue{key, count})
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Key < results[j].Key
+	})
+
+	outputFile := "mr-out-0"
+	file, err := os.Create(outputFile)
+	if err != nil {
+		log.Fatalf("Error creando archivo de salida: %v", err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for _, kv := range results {
+		fmt.Fprintf(writer, "%v %v\n", kv.Key, kv.Value)
+	}
+	writer.Flush()
 }
