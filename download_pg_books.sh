67ba0d332e2b1fad681648dce74099cb807293b9
lazcanoluca <lazcanoluca@gmail.com>
2025-09-23 16:06:33 -0300

    fix gutenberg downloader script

--- download_pg_books.sh
+++ download_pg_books.sh
@@ -6,5 +6,5 @@ mkdir -p books
 gutenberg search "subject:fantasy AND language:en" \
 | jq -r '.key' | head -n 100 | while read -r KEY; do
     echo "Exporting book $KEY..."
-    gutenberg text key:$KEY > my_books/pg-$KEY.txt
+    gutenberg text key:$KEY > books/pg-$KEY.txt
 done
